95f36dbf972d055d2cc8004dae8929a018fb42f1
Sylvain Lemiere <sylvain.lemiere@gojob.com>
2025-06-11 21:17:16 +0200

    fix

--- server.rb
+++ server.rb
@@ -2,7 +2,7 @@ require 'sinatra/base'
 require 'redis'
 
 class Server < Sinatra::Base
-  redis = Redis.new(url: ENV['REDIS_URL'] || 'redis://localhost:6379'})
+  redis = Redis.new(url: ENV['REDIS_URL'] || 'redis://localhost:6379')
   get "/add" do
     redis.increment("key")
   end
