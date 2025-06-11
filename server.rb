8ae2669da1ee9b1400ca13aee367408b1a4526f1
Sylvain Lemiere <sylvain.lemiere@gojob.com>
2025-06-11 21:21:27 +0200

    wip

--- server.rb
+++ server.rb
@@ -2,7 +2,10 @@ require 'sinatra/base'
 require 'redis'
 
 class Server < Sinatra::Base
-  redis = Redis.new(url: ENV['REDIS_URL'] || 'redis://localhost:6379')
+  redis = Redis.new(
+    url: ENV['REDIS_URL'] || 'redis://localhost:6379',
+    ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
+  )
   get "/add" do
     redis.increment("key")
   end
